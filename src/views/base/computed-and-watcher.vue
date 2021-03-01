ca11a789cd5ae1c078e1fd09a8056c8216ac433d
lugusliu <lugusliu@qq.com>
2021-03-01 23:41:39 +0800

    computed and watcher completed

--- src/views/base/computed-and-watcher.vue
+++ src/views/base/computed-and-watcher.vue
@@ -1,3 +1,84 @@
 <template>
-  <div class="caw">computed and watcher</div>
+  <div class="caw">
+    <p>
+      {{
+        msg
+          .split("")
+          .reverse()
+          .join("")
+      }}
+    </p>
+    <p>
+      计算属性是基于它们的响应式依赖进行缓存的。只在相关响应式依赖发生改变时它们才会重新求值
+    </p>
+    <p>计算属性：{{ reverseMsgByComputed }}</p>
+    <p>计算属性：{{ reverseMsgByComputed }}</p>
+    <p>方法：{{ reverseMsgByMethod() }}</p>
+    <p>方法：{{ reverseMsgByMethod() }}</p>
+    <p>fullName: {{ fullName }}</p>
+    <p>firstName: {{ firstName }}</p>
+    <p>lastName: {{ lastName }}</p>
+    <div>
+      <input placeholder="please ask a question" v-model="question" />
+      <p>your input is: {{ question }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+let timer = null;
+export default {
+  data() {
+    return {
+      msg: "Hello Vue",
+      firstName: "Lugus",
+      lastName: "Liu",
+      question: ""
+    };
+  },
+  computed: {
+    reverseMsgByComputed() {
+      console.log("execute computed reverse");
+      return this.msg
+        .split("")
+        .reverse()
+        .join("");
+    },
+    fullName: {
+      get() {
+        return this.firstName + " " + this.lastName;
+      },
+      set(newValue) {
+        const names = newValue.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      }
+    }
+  },
+  watch: {
+    question(newValue, oldValue) {
+      console.log("newValue", newValue);
+      console.log("oldValue", oldValue);
+    }
+  },
+  mounted() {
+    timer = setTimeout(() => {
+      this.fullName = "Harry Potter";
+    }, 2000);
+  },
+  beforeDestroy() {
+    if (timer) {
+      timer = null;
+    }
+  },
+  methods: {
+    reverseMsgByMethod() {
+      console.log("execute methods reverse");
+      return this.msg
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
+</script>
--- src/views/base/template-syntax.vue
+++ src/views/base/template-syntax.vue
@@ -1,3 +1,56 @@
 <template>
-  <div class="ts">template syntax</div>
+  <div class="ts">
+    <p>hello {{ msg }}</p>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+    <button :disabled="undefined">Button</button>
+    <p>模版中只能绑定单个表达式</p>
+    <p>
+      模版表达式都被放在沙盒中，只能访问全局变量的一个白名单:
+    </p>
+    <p>
+      Infinity<br />
+      undefined<br />
+      NaN<br />
+      isFinite<br />
+      isNaN<br />
+      parseFloat<br />
+      parseInt<br />
+      decodeURI<br />
+      decodeURIComponent<br />
+      encodeURI<br />
+      encodeURIComponent<br />
+      Math<br />
+      Number<br />
+      Date<br />
+      Array<br />
+      Object<br />
+      Boolean<br />
+      String<br />
+      RegExp<br />
+      Map<br />
+      Set<br />
+      JSON<br />
+      Intl<br />
+      require
+    </p>
+    <p>指令的值是预期的单个 Javascript 表达式（v-for 除外）</p>
+    <p>当表达式的值改变时，将其产生的连带影响，响应式的作用于 DOM</p>
+    <p>一些指令能够接收一个“参数”，在指令名称之后以冒号表示</p>
+    <p>
+      修饰符（modifier）是以半角句号 .
+      指明的特殊后缀，用于指出一个指令应该以特殊方式绑定
+    </p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "vue",
+      rawHtml: "<span style='color: red'>This should be red.</span>"
+    };
+  }
+};
+</script>
