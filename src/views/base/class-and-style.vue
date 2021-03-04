9260b3ee72c4c57214dd87efc06fd0cf01e92b78
lugusliu <lugusliu@qq.com>
2021-03-05 00:02:23 +0800

    update condition and list render

--- src/views/base/class-and-style.vue
+++ src/views/base/class-and-style.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="cas">
     <div class="static" :class="{ active: isActive, 'text-danger': hasError }">
-      class 绑定到一个对象上
-      <button @click="changeActiveToFalse">change isActive to false</button>
+      class 绑定到一个对象 { active: isActive, 'text-danger': hasError }
+      <button class="btn" @click="changeActiveToFalse">
+        isActive = false
+      </button>
     </div>
     <div class="static" :class="classObject">
-      class 绑定到一个 computed 计算属性上
+      class 绑定到一个 computed 计算属性，依赖于 isActive
     </div>
-    <div class="static" :class="arrayClass">
+    <div class="static" :class="[info, underline]">
       class 绑定到一个数组上
+    </div>
+    <div class="tips">
+      在一个自定义组件上使用 class property 时，这些 class
+      将被添加到该组件的跟元素上面，这个元素上已经存在的 class 不会被覆盖
     </div>
     <br />
     <div class="static" :style="{ color: activeColor, fontWeight: '600' }">
-      style 绑定到一个对象上
+      style 绑定到一个对象上 { color: activeColor, fontWeight: '600' }
+    </div>
+    <div class="static" :style="[baseStyles, overridingStyles]">
+      style 绑定到一个数组上 [baseStyles, overridingStyles]
     </div>
   </div>
 </template>
@@ -23,8 +32,15 @@ export default {
     return {
       isActive: true,
       hasError: false,
-      arrayClass: "info",
-      activeColor: "#52c41a"
+      info: "info",
+      underline: "underline",
+      activeColor: "#52c41a",
+      baseStyles: {
+        color: "#fa541c"
+      },
+      overridingStyles: {
+        fontWeight: "300"
+      }
     };
   },
   computed: {
@@ -44,6 +60,9 @@ export default {
 
 <style lang="scss" scoped>
 .cas {
+  .btn {
+    vertical-align: 4px;
+  }
   .static {
     font-size: 24px;
   }
@@ -55,6 +74,9 @@ export default {
   }
   .info {
     color: #bfbfbf;
+  }
+  .underline {
+    text-decoration: underline;
   }
 }
 </style>
--- src/views/base/condition-and-list-render.vue
+++ src/views/base/condition-and-list-render.vue
@@ -1,3 +1,52 @@
 <template>
-  <div class="calr">condition and list render</div>
+  <div class="calr">
+    <div class="vif">
+      <span class="static v-if-true" v-if="vif">vif is true</span>
+      <span class="static v-if-false" v-else>vif is false</span>
+      <button @click="changeVifToFalse">let vif = {{ !vif }}</button>
+    </div>
+    <div class="vshow">
+      <span class="static v-show-true" v-show="vshow">vshow is true</span>
+      <button @click="changeVshowToFalse">let vshow = {{ !vshow }}</button>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      vif: true,
+      vshow: true
+    };
+  },
+  methods: {
+    changeVifToFalse() {
+      this.vif = !this.vif;
+    },
+    changeVshowToFalse() {
+      this.vshow = !this.vshow;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.calr {
+  .static {
+    margin-right: 1em;
+  }
+  .vif {
+    margin-bottom: 20px;
+  }
+  .v-if-true {
+    background-color: #bae637;
+  }
+  .v-if-false {
+    background-color: #ff7875;
+  }
+  .v-show-true {
+    background-color: #b37feb;
+  }
+}
+</style>
