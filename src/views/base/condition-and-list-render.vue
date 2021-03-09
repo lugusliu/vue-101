e193c2383b668a5e1fef2dd82ef3e2657ca94c81
lugusliu <lugusliu@qq.com>
2021-03-09 23:41:14 +0800

    add event handle component

--- src/views/base/condition-and-list-render.vue
+++ src/views/base/condition-and-list-render.vue
@@ -9,6 +9,24 @@
       <span class="static v-show-true" v-show="vshow">vshow is true</span>
       <button @click="changeVshowToFalse">let vshow = {{ !vshow }}</button>
     </div>
+    <div class="vfor">
+      <span
+        class="color-block"
+        v-for="{ name, color, num } in colors"
+        :key="color"
+        :style="{ background: color }"
+        :title="name"
+        >{{ num }}</span
+      >
+      <button
+        v-for="(bool, key) in actions"
+        :key="key"
+        :disabled="!bool"
+        @click="handleAllAction(key)"
+      >
+        {{ key }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -17,7 +35,54 @@ export default {
   data() {
     return {
       vif: true,
-      vshow: true
+      vshow: true,
+      colors: [
+        {
+          name: "volcano",
+          color: "#ff9c6e",
+          num: 2
+        },
+        {
+          name: "lime",
+          color: "#d3f261",
+          num: 6
+        },
+        {
+          name: "sunset_orange",
+          color: "#ffc069",
+          num: 3
+        },
+        {
+          name: "polar_green",
+          color: "#95de64",
+          num: 7
+        },
+        {
+          name: "sunrise_yellow",
+          color: "#fff566",
+          num: 5
+        },
+        {
+          name: "calendula_gold",
+          color: "#ffd666",
+          num: 4
+        },
+        {
+          name: "cyan",
+          color: "#5cdbd3",
+          num: 8
+        }
+      ],
+      actions: {
+        push: true,
+        pop: false,
+        shift: false,
+        unshift: true,
+        splice: true,
+        sort: true,
+        reverse: true,
+        reset: false
+      }
     };
   },
   methods: {
@@ -26,6 +91,101 @@ export default {
     },
     changeVshowToFalse() {
       this.vshow = !this.vshow;
+    },
+    handleAllAction(key) {
+      if (key !== "reset") {
+        this.actions.reset = true;
+      }
+      this[key]();
+    },
+    push() {
+      this.actions.push = false;
+      this.actions.pop = true;
+      this.colors.push({
+        name: "daybreak_blue",
+        color: "#69c0ff",
+        num: 9
+      });
+    },
+    pop() {
+      this.actions.pop = false;
+      this.actions.push = true;
+      this.colors.pop();
+    },
+    unshift() {
+      this.actions.unshift = false;
+      this.actions.shift = true;
+      this.colors.unshift({
+        name: "dust_red",
+        color: "#ff7875",
+        num: 1
+      });
+    },
+    shift() {
+      this.actions.shift = false;
+      this.actions.unshift = true;
+      this.colors.shift();
+    },
+    splice() {
+      this.actions.splice = false;
+      this.colors.splice(2, 1);
+    },
+    sort() {
+      this.actions.sort = false;
+      this.colors.sort((a, b) => a.num - b.num);
+    },
+    reverse() {
+      this.colors.reverse();
+    },
+    reset() {
+      this.actions.reset = false;
+      this.colors = [
+        {
+          name: "volcano",
+          color: "#ff9c6e",
+          num: 2
+        },
+        {
+          name: "lime",
+          color: "#d3f261",
+          num: 6
+        },
+        {
+          name: "sunset_orange",
+          color: "#ffc069",
+          num: 3
+        },
+        {
+          name: "polar_green",
+          color: "#95de64",
+          num: 7
+        },
+        {
+          name: "sunrise_yellow",
+          color: "#fff566",
+          num: 5
+        },
+        {
+          name: "calendula_gold",
+          color: "#ffd666",
+          num: 4
+        },
+        {
+          name: "cyan",
+          color: "#5cdbd3",
+          num: 8
+        }
+      ];
+      this.actions = {
+        push: true,
+        pop: false,
+        shift: false,
+        unshift: true,
+        splice: true,
+        sort: true,
+        reverse: true,
+        reset: false
+      };
     }
   }
 };
@@ -39,6 +199,9 @@ export default {
   .vif {
     margin-bottom: 20px;
   }
+  .vshow {
+    margin-bottom: 20px;
+  }
   .v-if-true {
     background-color: #bae637;
   }
@@ -47,6 +210,21 @@ export default {
   }
   .v-show-true {
     background-color: #b37feb;
+  }
+  .vfor {
+    .color-block {
+      display: inline-block;
+      box-sizing: border-box;
+      text-align: center;
+      cursor: pointer;
+      border-radius: 4px;
+      line-height: 2em;
+      width: 2em;
+      margin-right: 0.5em;
+    }
+    button + button {
+      margin-left: 1em;
+    }
   }
 }
 </style>
--- src/views/base/event-handle.vue
+++ src/views/base/event-handle.vue
@@ -1,3 +1,70 @@
 <template>
-  <div class="eh">event handle</div>
+  <div class="eh">
+    <div class="eh-x">
+      普通事件处理方法 <button @click="handleNormal">点击</button>
+    </div>
+    <div class="eh-x">
+      内联处理器中的方法
+      <button @click="handleInline('event', $event)">点击</button>
+    </div>
+    <div class="eh-x">
+      <div @click="clickParent" class="parent-x">
+        <div @click.self="clickParentWidthSelf" class="parent">
+          <button @click="clickChild">冒泡测试</button>
+          <button @click.stop="clickChild">冒泡测试 - 带有 .stop 修饰符</button>
+        </div>
+      </div>
+    </div>
+    <div class="eh-x">
+      按键修饰符 .enter <input v-on:keyup.enter="submit" />
+    </div>
+    <div class="eh-x">
+      提交事件不再重载页面 .prevent
+      <form @submit.prevent="submit">
+        <input type="text" />
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleNormal(e) {
+      console.log("event", e);
+    },
+    handleInline(str, e) {
+      console.log(str, e);
+    },
+    clickChild() {
+      alert("child clicked");
+    },
+    clickParent() {
+      alert("parent clicked");
+    },
+    clickParentWidthSelf() {
+      alert("parent clicked width .self");
+    },
+    submit() {
+      alert("submit trigger");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.eh {
+  .eh-x + .eh-x {
+    margin-top: 20px;
+  }
+  .parent-x {
+    border: 10px solid #a0d911;
+    border-radius: 8px;
+  }
+  .parent {
+    padding: 20px 50px;
+    background-color: #fcffe6;
+    cursor: pointer;
+  }
+}
+</style>
